c04f9a15a2c9b9e782916a023b13239c25951b74
jessnmo <jessicamo92@gmail.com>
2023-04-30 17:36:06 +0200

    add global styling, create veggie options slide with updated api requst  return stored in localStorage

--- code/src/Components/Vegetable.jsx
+++ code/src/Components/Vegetable.jsx
@@ -1,5 +1,57 @@
+import { useEffect, useState } from 'react';
+import { Wrapper, Gradient, Card } from '../style/GlobalStyle';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+
 function Vegetable() {
-	return <div>Vegetable</div>;
+	const [vegetable, setVegetable] = useState([]);
+
+	useEffect(() => {
+		getVegetable();
+	}, []);
+
+	const getVegetable = async () => {
+		const checkVegetable = localStorage.getItem('vegetable');
+		if (checkVegetable) {
+			setVegetable(JSON.parse(checkVegetable));
+		} else {
+			const api = await fetch(
+				`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=15&tags=vegetarian`
+			);
+			const data = await api.json();
+			localStorage.setItem('vegetable', JSON.stringify(data.recipes));
+			setVegetable(data.recipes);
+		}
+	};
+
+	return (
+		<div>
+			<Wrapper>
+				<h2>Vegetarian Options</h2>
+				<Splide
+					options={{
+						perPage: 3,
+						arrows: false,
+						pagination: false,
+						drag: 'free',
+						gap: '5rem',
+					}}
+				>
+					{vegetable.map((veggieOption) => {
+						return (
+							<SplideSlide key={veggieOption.id}>
+								<Card>
+									<p>{veggieOption.title}</p>
+									<img src={veggieOption.image} alt={veggieOption.title} />
+									<Gradient />
+								</Card>
+							</SplideSlide>
+						);
+					})}
+				</Splide>
+			</Wrapper>
+		</div>
+	);
 }
 
 export default Vegetable;
--- code/src/pages/Pages.jsx
+++ code/src/pages/Pages.jsx
@@ -3,7 +3,6 @@ import HomePage from './HomePage';
 function Pages() {
 	return (
 		<div>
-			<h1>Hello</h1>
 			<HomePage />
 		</div>
 	);
